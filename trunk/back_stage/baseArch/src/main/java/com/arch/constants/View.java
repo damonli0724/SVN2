8307c23716b8c0ed53da013981314b6908e9886d
lkd <lkd@f89b1fc9-7c5e-455b-8322-85e5e281e6e7>
2015-12-09 14:21:40 +0000

    添加后台页面

--- trunk/back_stage/baseArch/src/main/java/com/arch/constants/View.java
+++ trunk/back_stage/baseArch/src/main/java/com/arch/constants/View.java
@@ -10,9 +10,9 @@ package com.arch.constants;
  */
 public interface View {
 	// =============================权限相关======================================
-	public static final String INDEX = "/security/index"; // 首页
+	public static final String INDEX = "/index"; // 首页
 	public static final String _403 = "/security/403";// 没有权限跳转页面
-	public static final String LOGIN = "/security/login"; // 登陆页面
+	public static final String LOGIN = "/login"; // 登陆页面
 	public static final String TIME_OUT_VIEW = "/security/timeout";  //session过期页面
 	//=========================================================================
 }
